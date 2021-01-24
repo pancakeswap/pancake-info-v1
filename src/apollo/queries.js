45dc098046409d2bf13492c4661ba3313b009626
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-01-24 15:43:04 +0100

    chore: GraphQL name

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -3,7 +3,7 @@ import { FACTORY_ADDRESS, BUNDLE_ID } from '../constants'
 
 export const SUBGRAPH_HEALTH = gql`
   query health {
-    indexingStatusForCurrentVersion(subgraphName: "wowswap") {
+    indexingStatusForCurrentVersion(subgraphName: "cakeswap") {
       synced
       health
       chains {
