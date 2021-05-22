2afc2336f2b8606b52f91630cfab853b6c3761d8
Boba Chef <thebobachef@gmail.com>
2021-05-22 11:42:11 -0700

    ft: more rice rebrand

--- src/components/Attribution/index.js
+++ src/components/Attribution/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Attribution = () => (
   <p className="attribution">
-    <a href="https://github.com/pancakeswap/pancakeswap-info" rel="noopener noreferrer" target="_blank">
+    <a href="https://github.com/rice=farm/rice-info" rel="noopener noreferrer" target="_blank">
       Github
     </a>{' '}
     |{' '}
-    <a href="https://pancakeswap.io" rel="noopener noreferrer" target="_blank">
-      pancakeswap
+    <a href="https://ricefarm.info" rel="noopener noreferrer" target="_blank">
+      ricefarm
     </a>{' '}
     |{' '}
     <a href="https://gifer.com/en/1Atv" rel="noopener noreferrer" target="_blank">
